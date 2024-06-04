ad6e35790ac07353d5606683eb80b3ae0ac1a89f
Clerisson Santos <clerisson.web@gmail.com>
2024-06-04 10:40:48 -0300

    adiciona esquema de tenant

---
+++ resources/views/urlerror.blade.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <!-- Page title -->
+    <title>Loja Fácil App</title>
+
+    <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+    <!--<link rel="shortcut icon" type="image/ico" href="favicon.ico" />-->
+
+    <!-- Vendor styles -->
+    <link rel="stylesheet" href="{{ url('vendor/fontawesome/css/font-awesome.css') }}">
+    <link rel="stylesheet" href="{{ url('vendor/metisMenu/dist/metisMenu.css') }}">
+    <link rel="stylesheet" href="{{ url('vendor/animate.css/animate.css') }}">
+    <link rel="stylesheet" href="{{ url('vendor/bootstrap/dist/css/bootstrap.css') }}">
+
+    <!-- App styles -->
+    <link rel="stylesheet" href="{{ url('fonts/pe-icon-7-stroke/css/pe-icon-7-stroke.css') }}">
+    <link rel="stylesheet" href="{{ url('fonts/pe-icon-7-stroke/css/helper.css') }}">
+    <link rel="stylesheet" href="{{ url('styles/style.css') }}">
+    @livewireStyles
+
+    <script src="{{ url('vendor/jquery/dist/jquery.min.js') }}"></script>
+    <script src="{{ url('vendor/jquery-ui/jquery-ui.min.js') }}"></script>
+    <script src="{{ url('vendor/slimScroll/jquery.slimscroll.min.js') }}"></script>
+    <script src="{{ url('vendor/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+    <script src="{{ url('vendor/jquery-flot/jquery.flot.js') }}"></script>
+    <script src="{{ url('vendor/jquery-flot/jquery.flot.resize.js') }}"></script>
+    <script src="{{ url('vendor/jquery-flot/jquery.flot.pie.js') }}"></script>
+    <script src="{{ url('vendor/flot.curvedlines/curvedLines.js') }}"></script>
+    <script src="{{ url('vendor/jquery.flot.spline/index.js') }}"></script>
+    <script src="{{ url('vendor/metisMenu/dist/metisMenu.min.js') }}"></script>
+    <script src="{{ url('vendor/iCheck/icheck.min.js') }}"></script>
+    <script src="{{ url('vendor/peity/jquery.peity.min.js') }}"></script>
+    <script src="{{ url('vendor/sparkline/index.js') }}"></script>
+
+    <script src="{{ url('scripts/homer.js') }}"></script>
+    <script src="{{ url('scripts/charts.js') }}"></script>
+    @livewireScripts
+
+
+    <style>
+        body {
+            color: #4ac8dc !important;
+        }
+        .bg-dark {
+            background-color: rgb(1,32,52) !important;
+        }
+
+        .bg-dark-medio {
+            background-color: rgb(1,52,87) !important;
+        }
+    </style>
+</head>
+<body class="blank bg-dark">
+
+<div class="login-container bg-dark">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="text-center m-b-md">
+                <img src="/images/logo-novo.png" height="300" width="300">
+                <h3>Você digitou o link errado, verifique e tente novamente!</h3>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <strong>Loja Fácil App</strong>
+        </div>
+    </div>
+</div>
+</body>
+</html>
