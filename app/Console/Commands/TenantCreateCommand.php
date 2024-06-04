c224e7a575a1ccfecc7699a2df07b28b38d279ab
Clerisson Santos <clerisson.web@gmail.com>
2024-06-04 10:54:59 -0300

    adiciona comanda make tenant

---
+++ app/Console/Commands/TenantCreateCommand.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Tenant;
+use Illuminate\Console\Command;
+
+class TenantCreateCommand extends Command
+{
+    protected $signature = 'tenant:create {name}';
+
+    protected $description = 'Criar um novo Tenant';
+
+    public function handle(): void
+    {
+        $tenant1 = Tenant::create(['id' => $this->argument('name')]);
+        $tenant1->domains()->create(['domain' => $this->argument('name') . '.lojafacilapp.com.br']);
+    }
+}
